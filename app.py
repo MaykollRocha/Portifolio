19a18ce1c106b5cddee4f010aa605e6e8f061307
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-06-03 19:22:47 -0230

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class Main():
             st.write(string)
             # PDF Download Button
             if st.button("Download Currículo"):
-                webbrowser.open_new_tab("https://drive.google.com/file/d/1el_CYCvMjbi2Sy7t9P4ZLLUQBLZlj5u6/view?usp=drive_link")
+                webbrowser.open_new_tab("https://drive.google.com/file/d/1-KDWmFwKy4qDxzKCxZkLxGCcPB40OfEB/view?usp=sharing")
         self.side_bar() 
 
     def side_bar(self):
