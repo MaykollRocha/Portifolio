22c572301d9b35253370d3fc4f93efa56c6d7db0
Maykoll Rocha <maykoll1412@gmail.com>
2024-10-12 20:00:15 -0400

    New

--- paginas/EstudosGuiados.py
+++ paginas/EstudosGuiados.py
@@ -1,22 +1,25 @@
 import streamlit as st
 
 
-def EstudoGuiados():
-    st.markdown("""
-                # Inicio na Ciencias de Dados
-                
-               Minha jornada em ciência de dados começou com o fascínio pela matemática, que logo se expandiu através das aulas de estatística aplicadas. O verdadeiro impulso veio durante a disciplina "Tópicos Avançados em Computação II", onde fui introduzido ao universo da ciência de dados utilizando Python. Nessa matéria, seguimos um livro que me marcou tanto que decidi adquiri-lo para aprofundar meus estudos. Desde então, venho me dedicando cada vez mais à área, encantado pela conexão entre matemática e ciência de dados e pela capacidade de transformar dados em insights valiosos.   
-                """)
+class EstudoGuiados():
+    def __init__(self) -> None:
+            
+        st.markdown("""
+                    # Inicio na Ciencias de Dados
+                    
+                Minha jornada em ciência de dados começou com o fascínio pela matemática, que logo se expandiu através das aulas de estatística aplicadas. O verdadeiro impulso veio durante a disciplina "Tópicos Avançados em Computação II", onde fui introduzido ao universo da ciência de dados utilizando Python. Nessa matéria, seguimos um livro que me marcou tanto que decidi adquiri-lo para aprofundar meus estudos. Desde então, venho me dedicando cada vez mais à área, encantado pela conexão entre matemática e ciência de dados e pela capacidade de transformar dados em insights valiosos.   
+                    """)
+        
+        st.markdown("""
+                    Durante essa matéria foi estudado todo um livro que é denominado Projeto de Ciência de Dados com Python de KLOSTERMAN, Stephen de 2019.
+                    O link a abaixo tem acesso ao oque foi feito e os tópicos estudos na matéria com um projeto final sendo posto na sessão pesquisa.  
+                    Tópicos:
+                    * Exploração de Dados  
+                    * Introdução ao Scikit-Learn e Avaliação do Modelo  
+                    * Detalhes da Regressão Logística e Exploração de Características  
+                    * Usando o gradiente descendente para reduzir a função custo
+                    * Árvores de Decisão e Florestas Aleatórias
+                    
+                    [Colaborátorio do Projeoto ](https://colab.research.google.com/drive/1qD9yALLQr6o0zQNJT36b5-wdv0YbysgP?usp=sharing)
+                    """)
     
-    st.markdown("""
-                Durante essa matéria foi estudado todo um livro que é denominado Projeto de Ciência de Dados com Python de KLOSTERMAN, Stephen de 2019.
-                O link a abaixo tem acesso ao oque foi feito e os tópicos estudos na matéria com um projeto final sendo posto na sessão pesquisa.  
-                Tópicos:
-                * Exploração de Dados  
-                * Introdução ao Scikit-Learn e Avaliação do Modelo  
-                * Detalhes da Regressão Logística e Exploração de Características  
-                * Usando o gradiente descendente para reduzir a função custo
-                * Árvores de Decisão e Florestas Aleatórias
-                
-                [Colaborátorio do Projeoto ](https://colab.research.google.com/drive/1qD9yALLQr6o0zQNJT36b5-wdv0YbysgP?usp=sharing)
-                """)
